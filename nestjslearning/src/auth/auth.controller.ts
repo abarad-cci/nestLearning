13ec6536789d5666c2ee13d8a70287ba44c1d0e8
akash.barad <akash.barad@CreativeCapsule.local>
2022-06-21 19:30:21 +0530

    DTOs and validation

--- nestjslearning/src/auth/auth.controller.ts
+++ nestjslearning/src/auth/auth.controller.ts
@@ -1,14 +1,16 @@
-import { Controller, Post, Req } from '@nestjs/common';
+import { Body, Controller, Post, Req } from '@nestjs/common';
 import { Request } from 'express';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signin')
-  signIn(@Req() req: Request) {
-    console.log(req.body);
+  signIn(@Body() dto: AuthDto) {
+    console.log(dto.email, dto.password);
+    console.log({ dto });
     return this.authService.signIn();
   }
   @Post('signup')
