38453970aa57970669d0f8d49d00072961a4a1f5
akash.barad <akash.barad@CreativeCapsule.local>
2022-06-22 05:20:38 +0530

    Basic authentication and Config Module

--- nestjslearning/src/app.module.ts
+++ nestjslearning/src/app.module.ts
@@ -1,13 +1,21 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config'; // added to root module
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    UserModule,
+    BookmarkModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- nestjslearning/src/prisma/prisma.service.ts
+++ nestjslearning/src/prisma/prisma.service.ts
@@ -1,15 +1,17 @@
 import { PrismaClient } from '.pnpm/@prisma+client@3.15.2_prisma@3.15.2/node_modules/.prisma/client';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config'; // injected from configModule
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(config: ConfigService) {
     super({
       datasources: {
         db: {
-          url: 'postgresql://postgres:123@localhost:5434/nest?schema=public',
+          url: config.get('DATABASE_URL'),
         },
       },
     });
+   // console.log(config.get('DATABASE_URL')); // getter function on config to get variable from .env file
   }
 }
