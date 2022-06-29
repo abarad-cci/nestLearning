bebf00c71f369fc08268f885a410b45495bbf038
akash.barad <akash.barad@CreativeCapsule.local>
2022-06-29 14:38:55 +0530

    Custom Param Decorator

--- nestjslearning/src/user/user.controller.ts
+++ nestjslearning/src/user/user.controller.ts
@@ -1,16 +1,20 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { User } from '@prisma/client';
 import { Request } from 'express';
 import { request } from 'http';
 import { userInfo } from 'os';
+import { GetUser } from 'src/auth/decorator';
+
 import { JwtGuard } from 'src/auth/guard';
 
+@UseGuards(JwtGuard) // simply means protect this route by the jwt strategy
+// putting a guard on top of a controller means all routes inside will use the guard
 @Controller('users')
 export class UserController {
-  @UseGuards(JwtGuard) // simply means protect this route by the jwt strategy
   @Get('me')
-  async getProfile(@Req() req: Request) {
+  async getProfile(@GetUser() user: User) {
     // console.log({ request: req.user });
-    return req.user;
+    return user;
   }
 }
