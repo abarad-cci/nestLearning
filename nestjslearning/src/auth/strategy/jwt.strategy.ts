6cd0a0fc107b25385e7fc7c6366fb2ebc2e978eb
akash.barad <akash.barad@CreativeCapsule.local>
2022-06-29 13:25:51 +0530

    Added: AuthGuard to protect Route

--- nestjslearning/src/auth/strategy/jwt.strategy.ts
+++ nestjslearning/src/auth/strategy/jwt.strategy.ts
@@ -2,11 +2,11 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(config: ConfigService) {
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(config: ConfigService, private prisma: PrismaService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -14,5 +14,18 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  validate() {}
+  async validate(payload: { sub: number; email: string }) {
+    // function extracts info from token and appends it to request.user
+    // console.log({ payload });
+
+    //validate if( user from token data = user from db )
+    const user = await this.prisma.user.findUnique({
+      where: { id: payload.sub },
+    });
+
+    // console.log(user);
+    // if (!user) return null;
+    delete user.hash;
+    return user; // if user === null it will throw unauthorized
+  }
 }
--- nestjslearning/src/user/user.controller.ts
+++ nestjslearning/src/user/user.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { request } from 'http';
+import { userInfo } from 'os';
 
 @Controller('users')
 export class UserController {
+  @UseGuards(AuthGuard('jwt')) // simply means protect this route by the jwt strategy
   @Get('me')
-  async getProfile() {
-    return 'this is user profile';
+  async getProfile(@Req() req: Request) {
+    // console.log({ request: req.user });
+    return req.user;
   }
 }
