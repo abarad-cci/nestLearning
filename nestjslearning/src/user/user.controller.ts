d9db581270188a89f10120cd09116a8d5ce5b4c9
akash.barad <akash.barad@CreativeCapsule.local>
2022-06-29 14:28:16 +0530

    Code refactor : added seperate class for AuthGuard ->  JwtGuard

--- nestjslearning/src/user/user.controller.ts
+++ nestjslearning/src/user/user.controller.ts
@@ -3,10 +3,11 @@ import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
 import { request } from 'http';
 import { userInfo } from 'os';
+import { JwtGuard } from 'src/auth/guard';
 
 @Controller('users')
 export class UserController {
-  @UseGuards(AuthGuard('jwt')) // simply means protect this route by the jwt strategy
+  @UseGuards(JwtGuard) // simply means protect this route by the jwt strategy
   @Get('me')
   async getProfile(@Req() req: Request) {
     // console.log({ request: req.user });
